eab3177a4ec0a4624b6d4f4224de59be0775a6cd
umvishnu <umvishnu.97@gmail.com>
2024-11-21 11:41:30 +0530

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,11 +1,11 @@
 questions = [
     {
-        "id": ,
+        "id": 1,
         "question": "Write short note on Certainty Equivalent Approach",
         "correct_answer": "This approach recognizes risk in capital budgeting analysis by adjusting estimated cash flows and employs risk free rate to discount the adjusted cash-flows. Under this method, the expected cash flows of the project are converted to equivalent riskless amounts. The greater the risk of an expected cash flow, the smaller the certainty equivalent values for receipts and longer the CE value for payment. This approach is superior to the risk adjusted discounted approach as it can measure risk more accurately. This is yet another approach for dealing with risk in capital budgeting to reduce the forecasts of cash flows to some conservative levels. In certainty Equivalent approach we incorporate risk to adjust the cash flows of a proposal so as to reflect the risk element. The certainty Equivalent approach adjusts future cash flows rather than discount rates. This approach explicitly recognizes risk, but the procedure for reducing the forecasts of cash flows is implicit and likely to be inconsistent from one investment to another."
     },
     {
-        "id": ,
+        "id": 2,
         "question": "What is the sensitivity analysis in Capital Budgeting?",
         "correct_answer": "Sensitivity analysis is used in Capital budgeting for more precisely measuring the risk. It helps in assessing information as to how sensitive are the estimated parameters of the project such as cash flows, discount rate, and the project life to the estimation errors. Future being always uncertain and estimations are always subject to error, sensitivity analysis takes care of estimation errors by using a number of possible outcomes in evaluating a project. The methodology adopted in sensitivity analysis is to evaluate a project by using a number of estimated cash flows so as to provide to the decision maker an insight into the variability of outcome. Thus, it is a technique of risk analysis which studies the responsiveness of a criterion of merit like NPV or IRR to variation in underlying factors like selling price, quantity sold, returns from an investment etc."
     }
